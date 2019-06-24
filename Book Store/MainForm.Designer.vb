7cd54b8a291d76df9d6af53ae942b70ab187b121
Maverick <murrayduke@rogers.com>
2019-06-23 23:17:31 -0400

    Add project files.

---
+++ Book Store/MainForm.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.btnAddToTotal = New System.Windows.Forms.Button()
+        Me.btnNewOrder = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Quantity:"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(89, 26)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(100, 23)
+        Me.txtQuantity.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Price:"
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(89, 67)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 23)
+        Me.txtPrice.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Total Due:"
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Location = New System.Drawing.Point(89, 108)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(83, 27)
+        Me.lblTotalDue.TabIndex = 5
+        Me.lblTotalDue.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'btnAddToTotal
+        '
+        Me.btnAddToTotal.Location = New System.Drawing.Point(220, 26)
+        Me.btnAddToTotal.Name = "btnAddToTotal"
+        Me.btnAddToTotal.Size = New System.Drawing.Size(100, 26)
+        Me.btnAddToTotal.TabIndex = 6
+        Me.btnAddToTotal.Text = "&Add to Total"
+        Me.btnAddToTotal.UseVisualStyleBackColor = True
+        '
+        'btnNewOrder
+        '
+        Me.btnNewOrder.Location = New System.Drawing.Point(220, 58)
+        Me.btnNewOrder.Name = "btnNewOrder"
+        Me.btnNewOrder.Size = New System.Drawing.Size(100, 26)
+        Me.btnNewOrder.TabIndex = 7
+        Me.btnNewOrder.Text = "&New Order"
+        Me.btnNewOrder.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(220, 87)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 26)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 164)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnNewOrder)
+        Me.Controls.Add(Me.btnAddToTotal)
+        Me.Controls.Add(Me.lblTotalDue)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Pennington Book Store"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents btnAddToTotal As Button
+    Friend WithEvents btnNewOrder As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Book Store/MainForm.vb
@@ -0,0 +1,54 @@
+﻿' Name Book Store
+' Purpose: School Project
+' Programmer: Murray Duke June 23th 2019
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class MainForm
+    Dim totalDue As Double
+
+    Private Sub BtnAddToTotal_Click(sender As Object, e As EventArgs) Handles btnAddToTotal.Click
+        ' Declare Veriables
+        Dim quantity As Integer
+        Dim price As Double
+
+        ' Assign veriables
+        Integer.TryParse(txtQuantity.Text, quantity)
+        Double.TryParse(txtPrice.Text, price)
+
+        If String.IsNullOrWhiteSpace(txtQuantity.Text) Then
+            MessageBox.Show("Please fill out quantity text box!", "Missing", MessageBoxButtons.OK)
+            txtQuantity.Focus()
+        ElseIf String.IsNullOrWhiteSpace(txtPrice.Text) Then
+            MessageBox.Show("Please fill out Price text box with an numeric only", "Missing", MessageBoxButtons.OK)
+            txtQuantity.Focus()
+        Else
+            totalDue += quantity * price
+            lblTotalDue.Text = totalDue.ToString("C")
+            ClearTextBoxes()
+        End If
+
+    End Sub
+    Private Sub ClearTextBoxes()
+        txtPrice.Text = String.Empty
+        txtQuantity.Text = String.Empty
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnNewOrder_Click(sender As Object, e As EventArgs) Handles btnNewOrder.Click
+        lblTotalDue.Text = String.Empty
+        totalDue = 0.0
+    End Sub
+
+    Private Sub NumbersOnly_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtQuantity.KeyPress, txtPrice.KeyPress
+        ' Accept only numbers, The period and the backspace key.
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> "." AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
